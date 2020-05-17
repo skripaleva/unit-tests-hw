66b10845a4242134ae156ab4725a96448caad272
markitosha <nataly_106@mail.ru>
2020-05-17 13:43:29 +0300

    Добавлены функции для 6 дз

---
+++ promiseAll.js
@@ -0,0 +1,24 @@
+/**
+ * Напишите функцию promiseAll(promises), поведение
+ * которой аналогично поведению Promise.all(promises).
+ *
+ * @param  {Promise[]} promises массив с исходными промисами
+ * @return {Promise}
+ */
+export const promiseAll = promises => new Promise((resolve, reject) => {
+    if (!promises.length) {
+        reject('empty arr');
+    }
+
+    promises.reduce((obj, item, idx) => {
+        item.then(res => {
+            obj.result[idx] = res;
+            obj.count -= 1;
+            if (!obj.count) {
+                resolve(obj.result);
+            }
+        }).catch(reject);
+
+        return obj;
+    }, { result: [], count: promises.length });
+});
